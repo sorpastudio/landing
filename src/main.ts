0ea1c7ff76d82ee03d87a8b6cd3cdc5101a12e34
Yevgeniy Korsukov <nickarsar@gmail.com>
2025-08-24 16:52:08 -0400

    Fix

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,26 @@
 import { createApp } from 'vue';
-import { setupI18n } from './i18n.js';
 import PrimeVue from 'primevue/config';
 
 import './style.css';
 import App from './App.vue';
 
+import { createI18n } from 'vue-i18n'
+import en from './locales/en.json';
+import es from './locales/es.json';
+import ru from './locales/ru.json';
+
 createApp(App)
     .use(PrimeVue, {
         unstyled: true,
     })
-    .use(setupI18n())
+    .use(createI18n({
+        legacy: false,
+        locale: 'en',
+        fallbackLocale: 'en',
+        messages: {
+            en,
+            es,
+            ru,
+        },
+    }))
     .mount('#app')
